a882c713691fd287a005f579db1de024d77ebfc0
Amol Gadage <AGadage@a10networks.com>
2023-10-17 11:23:09 +0530

    spring 3.1 with eureka client added

--- src/main/java/com/amolsoftwares/userservice/UserService/entities/User.java
+++ src/main/java/com/amolsoftwares/userservice/UserService/entities/User.java
@@ -1,8 +1,8 @@
 package com.amolsoftwares.userservice.UserService.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
